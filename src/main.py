bc8a168aa8481982f4690884c37cec8a15b58c5b
Kseniia Iarysh <kseniia.iarysh@orioninc.com>
2026-01-08 12:27:20 -0300

    reeng

--- src/main.py
+++ src/main.py
@@ -59,7 +59,11 @@ def build_app():
     app.add_handler(CommandHandler("cancel", cmd_cancel))
 
     # этот handler должен идти ДО обычного текстового
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, contact_message_handler))
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, contact_message_handler, block=False)
+    )
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, on_text))
 
     # 3) messages
     app.add_handler(MessageHandler(filters.PHOTO, on_photo))
