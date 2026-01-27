97cd42dd6ef51b1c12a222f1786f2c27b23d7a3a
Kseniia Iarysh <kseniia.iarysh@orioninc.com>
2026-01-27 13:07:04 -0300

    reeng

--- src/i18n/t.py
+++ src/i18n/t.py
@@ -1,7 +1,7 @@
 from .locales.ru import RU
 from .locales.pt_br import PT_BR
 
-MAP = {"ru": RU, "pt_BR": PT_BR}
+MAP = {"ru": RU, "pt": PT_BR}  # <-- вот это важно
 
 def t(key: str, lang: str = "ru", **kwargs) -> str:
     d = MAP.get(lang, RU)
